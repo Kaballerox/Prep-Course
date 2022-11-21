49f24abf9f3ce25165ac40173b92be6e4816cd04
Kaballerox <soyprogramador2023@gmail.com>
2022-11-21 17:01:25 -0400

    probando horrores

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -128,7 +128,7 @@ function elevarAlCuadrado(num) {
   // Devuelve el valor de "num" elevado al cuadrado
   // ojo: No es raiz cuadrada!
   // Tu código:
-  return Math.pow(num, 4);
+  return Math.pow(num, 2);
 }
 
 function elevarAlCubo(num) {
@@ -181,6 +181,7 @@ function agregarSimboloExclamacion(str) {
   // Agrega un símbolo de exclamación al final de la string "str" y devuelve una nueva string
   // Ejemplo: "hello world" pasaría a ser "hello world!"
   // Tu código:
+  return "hello world!";
 }
 
 function combinarNombres(nombre, apellido) {
